b03b366e0669dcee54e5eda793982f4ce973e079
chiging <80450404+chiging@users.noreply.github.com>
2021-03-11 16:06:15 +0800

    webpage

---
+++ index_center.js
@@ -0,0 +1,46 @@
+document.writeln("<body>");
+document.writeln("<div align=\"center\">");
+document.writeln("  <table width=\"100%\" border=\"0\" cellpadding=\"2\" cellspacing=\"0\">");
+document.writeln("    <tr>");
+document.writeln("       <th width=\"100%\" bgcolor=\"#FAFAFA\" scope=\"col\"><table border=\"0\" cellspacing=\"0\" cellpadding=\"0\">");
+document.writeln("            <tr>");
+document.writeln("            ");
+document.writeln(" <div id=\"abgneBlock\">");
+document.writeln("  <div id=\"player\">");
+document.writeln("    <ul class=\"list\">");
+document.writeln("     <li><a target=\"_blank\" href=\"http:\/\/promote.pscnet.com.tw\/canteen\/\"><img src=\"new_ad\/ber.jpg\" \/><\/a><\/li>");
+/* 新增一個大廣告
+document.writeln("     <li><a target=\"_blank\" href=\"http:\/\/www.pfcf.com.tw\/ezcatfiles\/cust\/img\/img\/34\/0414_taichung.html\"><img src=\"new_ad\/future0414_taichung.gif\" \/><\/a><\/li>      ");
+*/
+/*101.08.20 add
+document.writeln("     <li><a target=\"_blank\" href=\"http:\/\/promote.pscnet.com.tw\/WarrantGame2012\/Register.aspx\"><img src=\"new_ad\/warrant0820.gif\" \/><\/a><\/li>      ");
+101.04.30 add*/
+/*
+document.writeln("<li><a target=\"_blank\" href=\"http:\/\/www.ezmoney.com.tw\/activity\/2012asianbric\/index.htm\"><img src=\"new_ad\/ezmoney_0801.jpg\" \/><\/a><\/li>");
+*/
+document.writeln("     <li><a target=\"_blank\" href=\"http:\/\/www.pscnet.com.tw\/new_ad\/ti\/index.htm\"><img src=\"new_ad\/20130118.gif\" \/><\/a><\/li>      ");
+
+/*
+
+document.writeln("     <li><a target=\"_blank\" href=\"http:\/\/www.pscnet.com.tw\/new_ad\/2012love\/index.html\"><img src=\"new_ad\/banner_20121012.gif\" \/><\/a><\/li>      ");
+*/
+
+document.writeln("     <li><a target=\"_blank\" href=\"http:\/\/promote.pscnet.com.tw\/WarrantPromote2012\/Default.aspx\"><img src=\"new_ad\/20121112.gif\" \/><\/a><\/li>      ");
+
+document.writeln("     <li><a target=\"_blank\" href=\"http:\/\/www.ezmoney.com.tw\/activity\/2013q1event\"><img src=\"new_ad\/20130104.jpg\" \/><\/a><\/li>      ");
+/*
+document.writeln("     <li><a target=\"_blank\" href=\"http:\/\/at.tse.com.tw\/invest01\/at_reg.asp\"><img src=\"new_ad\/20121206.gif\" \/><\/a><\/li>      ");
+*/
+
+document.writeln("     <li><a target=\"_blank\" href=\"http:\/\/www.pscnet.com.tw\/event\/iphone5\/index.html\"><img src=\"images\/20130104.jpg\" \/><\/a><\/li>      ");
+
+
+document.writeln("     <\/ul>");
+document.writeln("  <\/div>");
+document.writeln("<\/div>");
+document.writeln("            <\/tr>");
+document.writeln("      <\/table><\/th>");
+document.writeln("    <\/tr>");
+document.writeln("  <\/table>");
+document.writeln("<\/div>");
+document.writeln("<\/body>")
